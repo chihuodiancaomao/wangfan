30605e4187d1588f2133eab715dc38592f0ec4b9
wangfan <1184075899@qq.com>
2019-03-12 23:32:41 +0800

    jjhjh

--- src/main/java/com/wf/SpringBootMybatisApplication.java
+++ src/main/java/com/wf/SpringBootMybatisApplication.java
@@ -3,14 +3,23 @@ package com.wf;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("com.wf.dao")
 @SpringBootApplication
 
-public class SpringBootMybatisApplication {
+public class SpringBootMybatisApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMybatisApplication.class, args);
+	}
+	
+	@Override//为了打包springboot项目
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		
+		return builder.sources(this.getClass());
 	}
 
 }
